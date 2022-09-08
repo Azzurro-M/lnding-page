855d1a1d0cb29b008d7787c0421e10c2d3bf08a1
Marianna <azzurromarianna@gmail.com>
2022-09-08 20:23:00 +0100

    implemented navbar dynamically

--- js/app.js
+++ js/app.js
@@ -55,105 +55,105 @@
 
 // navigation adding HTML
 
-function createShowcase() {
-  let ul = document.getElementById("navbar__list");
-  let li = document.createElement("li");
-  // li.textContent = 'Showcase';
-  li.setAttribute("id", "#element1");
-  ul.appendChild(li);
-}
+// function createShowcase() {
+//   let ul = document.getElementById("navbar__list");
+//   let li = document.createElement("li");
+//   // li.textContent = 'Showcase';
+//   li.setAttribute("id", "#element1");
+//   ul.appendChild(li);
+// }
 
-createShowcase();
+// createShowcase();
 
-function showLink() {
-  let liShow = document.getElementById("#element1");
-  let a = document.createElement("a");
-  a.setAttribute("href", "#section1");
-  a.textContent = "Showcase";
-  liShow.appendChild(a);
-}
+// function showLink() {
+//   let liShow = document.getElementById("#element1");
+//   let a = document.createElement("a");
+//   a.setAttribute("href", "#section1");
+//   a.textContent = "Showcase";
+//   liShow.appendChild(a);
+// }
 
-showLink();
+// showLink();
 
-function createHowWeDoIt() {
-  let ul = document.getElementById("navbar__list");
-  let li = document.createElement("li");
-  // li.textContent = 'How We Do It';
-  li.setAttribute("id", "#element2");
-  ul.appendChild(li);
-}
+// function createHowWeDoIt() {
+//   let ul = document.getElementById("navbar__list");
+//   let li = document.createElement("li");
+//   // li.textContent = 'How We Do It';
+//   li.setAttribute("id", "#element2");
+//   ul.appendChild(li);
+// }
 
-createHowWeDoIt();
+// createHowWeDoIt();
 
-function howLink() {
-  let liHow = document.getElementById("#element2");
-  let a = document.createElement("a");
-  a.setAttribute("href", "#section2");
-  a.textContent = "How We Do It";
-  liHow.appendChild(a);
-}
+// function howLink() {
+//   let liHow = document.getElementById("#element2");
+//   let a = document.createElement("a");
+//   a.setAttribute("href", "#section2");
+//   a.textContent = "How We Do It";
+//   liHow.appendChild(a);
+// }
 
-howLink();
+// howLink();
 
-function createOurProject() {
-  let ul = document.getElementById("navbar__list");
-  let li = document.createElement("li");
-  // li.textContent = 'Our Project';
-  li.setAttribute("id", "#element3");
-  ul.appendChild(li);
-}
+// function createOurProject() {
+//   let ul = document.getElementById("navbar__list");
+//   let li = document.createElement("li");
+//   // li.textContent = 'Our Project';
+//   li.setAttribute("id", "#element3");
+//   ul.appendChild(li);
+// }
 
-createOurProject();
+// createOurProject();
 
-function projectLink() {
-  let liPro = document.getElementById("#element3");
-  let a = document.createElement("a");
-  a.setAttribute("href", "#section3");
-  a.textContent = "Our Projects";
-  liPro.appendChild(a);
-}
+// function projectLink() {
+//   let liPro = document.getElementById("#element3");
+//   let a = document.createElement("a");
+//   a.setAttribute("href", "#section3");
+//   a.textContent = "Our Projects";
+//   liPro.appendChild(a);
+// }
 
-projectLink();
+// projectLink();
 
-function createTestimonials() {
-  let ul = document.getElementById("navbar__list");
-  let li = document.createElement("li");
-  // li.textContent = 'Testimonials';
-  li.setAttribute("id", "#element4");
-  ul.appendChild(li);
-}
+// function createTestimonials() {
+//   let ul = document.getElementById("navbar__list");
+//   let li = document.createElement("li");
+//   // li.textContent = 'Testimonials';
+//   li.setAttribute("id", "#element4");
+//   ul.appendChild(li);
+// }
 
-createTestimonials();
+// createTestimonials();
 
-function testimonialsLink() {
-  let liTest = document.getElementById("#element4");
-  let a = document.createElement("a");
-  a.setAttribute("href", "#section4");
-  a.textContent = "Testimonials";
-  liTest.appendChild(a);
-}
+// function testimonialsLink() {
+//   let liTest = document.getElementById("#element4");
+//   let a = document.createElement("a");
+//   a.setAttribute("href", "#section4");
+//   a.textContent = "Testimonials";
+//   liTest.appendChild(a);
+// }
 
-testimonialsLink();
+// testimonialsLink();
 
-function createSubscribe() {
-  let ul = document.getElementById("navbar__list");
-  let li = document.createElement("li");
-  // li.textContent = 'Subscribe';
-  li.setAttribute("id", "#element5");
-  ul.appendChild(li);
-}
+// function createSubscribe() {
+//   let ul = document.getElementById("navbar__list");
+//   let li = document.createElement("li");
+//   // li.textContent = 'Subscribe';
+//   li.setAttribute("id", "#element5");
+//   ul.appendChild(li);
+// }
 
-createSubscribe();
+// createSubscribe();
 
-function subLink() {
-  let liSub = document.getElementById("#element5");
-  let a = document.createElement("a");
-  a.setAttribute("href", "#section5");
-  a.textContent = "Subscribe";
-  liSub.appendChild(a);
-}
+// function subLink() {
+//   let liSub = document.getElementById("#element5");
+//   let a = document.createElement("a");
+//   a.setAttribute("href", "#section5");
+//   a.textContent = "Subscribe";
+//   liSub.appendChild(a);
+// }
 
-subLink();
+// subLink();
 
 //event listener for anchor (link)
 
@@ -170,51 +170,30 @@ document.querySelectorAll(".navbar__menu a").forEach((a) => {
   });
 });
 
-// adding H2 elements
+// ADDING NAV + SECTION TITLE
 
-function createTitle() {
-  let sectionTitle = document.querySelector("#section1");
-  let h2 = document.createElement("h2");
-  h2.textContent = "Our Showcase";
-  h2.setAttribute("id", "our_showcase");
-  sectionTitle.appendChild(h2);
-  sectionTitle.insertAdjacentElement("afterbegin", h2);
-}
+const sections = document.querySelectorAll("section");
+const navbarList = document.querySelector("#navbar__list");
+sections.forEach((section) => {
+  const li = document.createElement("li");
+  const h2 = document.createElement("h2");
+  h2.textContent = section.getAttribute("data-nav");
+  li.appendChild(h2);
+  navbarList.appendChild(li);
 
-createTitle();
+  h2.addEventListener("click", () => {
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "start",
+    });
+  });
 
-function howWeDoIt() {
-  let sectionTitle = document.querySelector("#section2");
-  let h2 = document.createElement("h2");
-  h2.textContent = "How We Do It";
-  h2.setAttribute("id", "how_we_do_it");
-  sectionTitle.appendChild(h2);
-  sectionTitle.insertAdjacentElement("afterbegin", h2);
-}
-
-howWeDoIt();
-
-function ourProjects() {
-  let sectionTitle = document.querySelector("#section3");
-  let h2 = document.createElement("h2");
-  h2.textContent = "Our Projects";
-  h2.setAttribute("id", "our_projects");
-  sectionTitle.appendChild(h2);
-  sectionTitle.insertAdjacentElement("afterbegin", h2);
-}
-
-ourProjects();
-
-function testimonials() {
-  let sectionTitle = document.querySelector("#section4");
-  let h2 = document.createElement("h2");
-  h2.textContent = "Testimonials";
-  h2.setAttribute("id", "testimonials");
-  sectionTitle.appendChild(h2);
-  sectionTitle.insertAdjacentElement("afterbegin", h2);
-}
-
-testimonials();
+  const h1 = document.createElement("h1");
+  h1.textContent = h2.textContent;
+  h1.classList.add("section-title");
+  section.prepend(h1);
+});
 
 //scroll and highlight nav events
 
@@ -223,7 +202,6 @@ document.querySelector("form").addEventListener("submit", (e) => {
   alert("The Form Has Been Submitted Successfully");
 });
 
-const sections = document.querySelectorAll("section");
 const navLi = document.querySelectorAll(".navbar__menu ul li");
 
 addEventListener("scroll", () => {
@@ -236,34 +214,29 @@ addEventListener("scroll", () => {
       current = section.getAttribute("id");
     }
   });
+});
 
-  console.log(current);
-
-  navLi.forEach((li) => {
-    li.classList.remove("active");
-    if (li.querySelector("a").getAttribute("href") == "#" + current) {
-      li.classList.add("active");
-    }
-  });
+navLi.forEach((li) => {
+  li.classList.remove("active");
+  if () {
+    li.classList.add("active");
+  }
 });
 
 //mobile navigation
 
 const hambIcon = document.querySelector("#hamburger");
-const list = mobileList;
+const navbarMenu = document.querySelector(".navbar__menu");
 
-list.forEach((title) => {
-  const li = document.createElement("li");
-  li.textContent = list.title;
-  const a = document.createElement("a");
-  li.appendChild(a);
-});
+// list.forEach((title) => {
+//   const li = document.createElement("li");
+//   li.textContent = list.title;
+//   const a = document.createElement("a");
+//   li.appendChild(a);
+// });
 
 hambIcon.addEventListener("click", (e) => {
-  const nav = document.querySelector("nav");
-  console.log(nav);
-
-  nav.classList.toggle("showNav");
+  navbarMenu.classList.toggle("navmenu-active");
+  navbarList.classList.toggle("navlist-active");
+  console.log("hello");
 });
-
-console.log(list);
--- js/mobileNav.js
+++ js/mobileNav.js
@@ -1,31 +1,31 @@
-const mobileList = [
-  {
-    title: "Showcase",
-    hash: "#element1",
-    link: "#section1",
-  },
+// const mobileList = [
+//   {
+//     title: "Showcase",
+//     hash: "#element1",
+//     link: "#section1",
+//   },
 
-  {
-    title: "How We Do It",
-    hash: "#element2",
-    link: "#section2",
-  },
+//   {
+//     title: "How We Do It",
+//     hash: "#element2",
+//     link: "#section2",
+//   },
 
-  {
-    title: "Our Projects",
-    hash: "#element3",
-    link: "#section3",
-  },
+//   {
+//     title: "Our Projects",
+//     hash: "#element3",
+//     link: "#section3",
+//   },
 
-  {
-    title: "Testimonials",
-    hash: "#element4",
-    link: "#section4",
-  },
+//   {
+//     title: "Testimonials",
+//     hash: "#element4",
+//     link: "#section4",
+//   },
 
-  {
-    title: "Subscribe",
-    hash: "#element5",
-    link: "#section5",
-  },
-];
+//   {
+//     title: "Subscribe",
+//     hash: "#element5",
+//     link: "#section5",
+//   },
+// ];
