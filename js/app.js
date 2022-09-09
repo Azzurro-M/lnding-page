33fbff5f0462f840a156a43d888d630137432af4
Marianna <azzurromarianna@gmail.com>
2022-09-09 18:04:31 +0100

    completed javascript

--- js/app.js
+++ js/app.js
@@ -1,179 +1,21 @@
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
- */
+// //SMOOTH BEHAVIOUR
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
- */
+// document.querySelectorAll(".navbar__menu a").forEach((a) => {
+//   a.addEventListener("click", (event) => {
+//     event.preventDefault();
+//     const id = a.getAttribute("href");
+//     const section = document.querySelector(id);
+//     section.scrollIntoView({
+//       behavior: "smooth",
+//     });
+//   });
+// });
 
-/**
- * Define Global Variables
- *
- */
-
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
-
-// build the nav
-
-// Add class 'active' to section when near top of viewport
-
-// Scroll to anchor ID using scrollTO event
-
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
-
-// navigation adding HTML
-
-// function createShowcase() {
-//   let ul = document.getElementById("navbar__list");
-//   let li = document.createElement("li");
-//   // li.textContent = 'Showcase';
-//   li.setAttribute("id", "#element1");
-//   ul.appendChild(li);
-// }
-
-// createShowcase();
-
-// function showLink() {
-//   let liShow = document.getElementById("#element1");
-//   let a = document.createElement("a");
-//   a.setAttribute("href", "#section1");
-//   a.textContent = "Showcase";
-//   liShow.appendChild(a);
-// }
-
-// showLink();
-
-// function createHowWeDoIt() {
-//   let ul = document.getElementById("navbar__list");
-//   let li = document.createElement("li");
-//   // li.textContent = 'How We Do It';
-//   li.setAttribute("id", "#element2");
-//   ul.appendChild(li);
-// }
-
-// createHowWeDoIt();
-
-// function howLink() {
-//   let liHow = document.getElementById("#element2");
-//   let a = document.createElement("a");
-//   a.setAttribute("href", "#section2");
-//   a.textContent = "How We Do It";
-//   liHow.appendChild(a);
-// }
-
-// howLink();
-
-// function createOurProject() {
-//   let ul = document.getElementById("navbar__list");
-//   let li = document.createElement("li");
-//   // li.textContent = 'Our Project';
-//   li.setAttribute("id", "#element3");
-//   ul.appendChild(li);
-// }
-
-// createOurProject();
-
-// function projectLink() {
-//   let liPro = document.getElementById("#element3");
-//   let a = document.createElement("a");
-//   a.setAttribute("href", "#section3");
-//   a.textContent = "Our Projects";
-//   liPro.appendChild(a);
-// }
-
-// projectLink();
-
-// function createTestimonials() {
-//   let ul = document.getElementById("navbar__list");
-//   let li = document.createElement("li");
-//   // li.textContent = 'Testimonials';
-//   li.setAttribute("id", "#element4");
-//   ul.appendChild(li);
-// }
-
-// createTestimonials();
-
-// function testimonialsLink() {
-//   let liTest = document.getElementById("#element4");
-//   let a = document.createElement("a");
-//   a.setAttribute("href", "#section4");
-//   a.textContent = "Testimonials";
-//   liTest.appendChild(a);
-// }
-
-// testimonialsLink();
-
-// function createSubscribe() {
-//   let ul = document.getElementById("navbar__list");
-//   let li = document.createElement("li");
-//   // li.textContent = 'Subscribe';
-//   li.setAttribute("id", "#element5");
-//   ul.appendChild(li);
-// }
-
-// createSubscribe();
-
-// function subLink() {
-//   let liSub = document.getElementById("#element5");
-//   let a = document.createElement("a");
-//   a.setAttribute("href", "#section5");
-//   a.textContent = "Subscribe";
-//   liSub.appendChild(a);
-// }
-
-// subLink();
-
-//event listener for anchor (link)
-
-//SMOOTH BEHAVIOUR
-
-document.querySelectorAll(".navbar__menu a").forEach((a) => {
-  a.addEventListener("click", (event) => {
-    event.preventDefault();
-    const id = a.getAttribute("href");
-    const section = document.querySelector(id);
-    section.scrollIntoView({
-      behavior: "smooth",
-    });
-  });
-});
-
-// ADDING NAV + SECTION TITLE
+// ADDING NAV + SECTION TITLE + CONNECTING NAV TO SESSIONS AND SMOOTH BEHAVIOUR
 
 const sections = document.querySelectorAll("section");
 const navbarList = document.querySelector("#navbar__list");
+
 sections.forEach((section) => {
   const li = document.createElement("li");
   const h2 = document.createElement("h2");
@@ -195,45 +37,55 @@ sections.forEach((section) => {
   section.prepend(h1);
 });
 
-//scroll and highlight nav events
+//FORM SUBMISSION EVENT
 
 document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault();
   alert("The Form Has Been Submitted Successfully");
 });
 
+//SCROLL HIGHLIGHT
+
 const navLi = document.querySelectorAll(".navbar__menu ul li");
 
 addEventListener("scroll", () => {
   let current = "";
+  let searching = true;
   sections.forEach((section) => {
-    const sectionTop = section.offsetTop;
+    const sectionPosition = section.getBoundingClientRect();
     const sectionHeight = section.clientHeight;
 
-    if (scrollY + 68 > sectionTop) {
-      current = section.getAttribute("id");
+    if (65 < sectionPosition.bottom && searching) {
+      searching = false;
+      section.classList.add("active");
+      navLi.forEach((li) => {
+        if (section.dataset.nav === li.textContent) {
+          li.classList.add("active");
+        }
+      });
+    } else {
+      section.classList.remove("active");
+      navLi.forEach((li) => {
+        if (section.dataset.nav === li.textContent) {
+          li.classList.remove("active");
+        }
+      });
     }
   });
 });
 
-navLi.forEach((li) => {
-  li.classList.remove("active");
-  if () {
-    li.classList.add("active");
-  }
-});
+// navLi.forEach((li) => {
+//   li.classList.remove("active");
+//   if () {
+//     li.classList.add("active");
+//   }
+
+// });
 
 //mobile navigation
 
 const hambIcon = document.querySelector("#hamburger");
 const navbarMenu = document.querySelector(".navbar__menu");
-
-// list.forEach((title) => {
-//   const li = document.createElement("li");
-//   li.textContent = list.title;
-//   const a = document.createElement("a");
-//   li.appendChild(a);
-// });
 
 hambIcon.addEventListener("click", (e) => {
   navbarMenu.classList.toggle("navmenu-active");
